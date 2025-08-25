1b736042aa05720104899ec163d28e146fa677e0
Iurii Malchenko <i@yurique.com>
2025-08-25 22:43:42 +0200

    testing package.swift

--- Package.swift
+++ Package.swift
@@ -1,18 +1,55 @@
 // swift-tools-version: 5.9
-import PackageKit
+import PackageDescription
+
 let package = Package(
-  name: "LibreTransmitterSPM",
-  platforms: [.iOS(.v15)],
-  products: [
-    .library(name: "LibreTransmitter", targets: ["LibreTransmitter"]),
-    .library(name: "RawGlucose", targets: ["RawGlucose"]),
-  ],
-  targets: [
-      .binaryTarget(
+    name: "LibreTransmitterSPM",
+    defaultLocalization: "en",
+    platforms: [.iOS(.v15)],
+
+    dependencies: [
+            .package(url: "https://github.com/LoopKit/LoopKit.git", branch: "dev")
+    ],
+
+    products: [
+        .library(name: "LibreTransmitter", targets: ["LibreTransmitter"]),
+        .library(name: "LibreTransmitterUI", targets: ["LibreTransmitterUI"]),
+    ],
+
+    targets: [
+        // Prebuilt binary
+        .binaryTarget(
             name: "RawGlucose",
-            path: "RawGlucose.xcframework"),
-      .target(name: "LibreTransmitter",
-            dependencies: ["RawGlucose"],
-            path: "LibreTransmitter")
-  ]
+            path: "RawGlucose.xcframework"
+        ),
+
+        // Internal code modules (one per folder)
+        .target(name: "Common", path: "Common"),
+        .target(name: "Bluetooth", dependencies: ["Common"], path: "Bluetooth"),
+        .target(name: "LibreSensor", dependencies: ["Common","Bluetooth"], path: "LibreSensor"),
+
+        // Main library
+        .target(
+            name: "LibreTransmitter",
+            dependencies: [
+                "Common",
+                "Bluetooth",
+                "LibreSensor",
+                "RawGlucose",
+                .product(name: "LoopKit", package: "LoopKit"),
+                .product(name: "LoopKitUI", package: "LoopKit")
+            ],
+            path: "LibreTransmitter"
+            // resources: [.process("Resources")] // add if you have bundled assets
+        ),
+
+        // UI library
+        .target(
+            name: "LibreTransmitterUI",
+            dependencies: [
+                "LibreTransmitter",
+            ],
+            path: "LibreTransmitterUI"
+            // resources: [.process("Resources")]
+        ),
+    ]
 )
